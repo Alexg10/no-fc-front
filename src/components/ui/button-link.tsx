13e811f6ca12b20abecd1ffdb5f0b23e930993d7
Alexg10 <alexandre.guerard6@gmail.com>
2026-02-09 19:30:05 +0100

    Add lottie anim + update ripped paper

--- src/components/ui/button-link.tsx
+++ src/components/ui/button-link.tsx
@@ -3,9 +3,11 @@
 import { Loader2 } from "lucide-react";
 import Link from "next/link";
 
-import { GlobeIcon } from "@/components/icons/globe-icon";
 import { cn } from "@/lib/utils";
 import { cva, type VariantProps } from "class-variance-authority";
+import { useState } from "react";
+import Lottie from "react-lottie-player";
+import EyeButton from "../../../public/lotties/eye-button.json";
 
 const buttonLinkVariants = cva(
   "flex items-center gap-4 border border-black p-2 !text-obviously uppercase group w-fit",
@@ -39,13 +41,26 @@ export function ButtonLink({
   className,
   loading = false,
 }: ButtonLinkProps) {
+  const [isHover, setIsHover] = useState(false);
+
   return (
     <Link
       href={href}
       className={cn(buttonLinkVariants({ variant, className }))}
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
     >
       <div className="flex items-center gap-2 border border-white p-2 px-4">
-        {hasIcon && <GlobeIcon className="w-5 h-5 lg:w-6 lg:h-6" />}
+        {hasIcon && (
+          <span className="size-5 bg-black">
+            <Lottie
+              animationData={EyeButton}
+              loop
+              play={isHover}
+              className=""
+            />
+          </span>
+        )}
         <div className="-translate-y-px">{children}</div>
         {loading && (
           <Loader2 className="ml-2 size-5 animate-spin" aria-hidden="true" />
