978a84f4355039bbf6e5c58850b039198851cd4b
Alexg10 <alexandre.guerard6@gmail.com>
2026-04-09 01:11:11 +0200

    Refactor PlayVideoButton component to accept custom className prop for improved styling flexibility; update VideoFullWidthBlock to disable pointer events on the play button when necessary.

--- src/components/common/play-video-button.tsx
+++ src/components/common/play-video-button.tsx
@@ -2,24 +2,30 @@
 
 import { PlayIcon } from "@/components/icons/play-icon";
 import { useBreakpoints } from "@/hooks/useBreakpoints";
+import { cn } from "@/lib/utils";
 import Marquee from "react-fast-marquee";
 
 interface PlayVideoButtonProps {
   onClick: () => void;
   ariaLabel?: string;
   marqueeIsPlaying?: boolean;
+  className?: string;
 }
 
 export function PlayVideoButton({
   onClick,
   ariaLabel = "Play video",
   marqueeIsPlaying = false,
+  className,
 }: PlayVideoButtonProps) {
   const isUnderTablet = useBreakpoints().isUnderTablet;
   return (
     <button
       onClick={onClick}
-      className="bg-black cursor-pointer p-2 flex absolute z-20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-center text-2xl font-bold duration-300 max-w-[185px]"
+      className={cn(
+        "bg-black cursor-pointer p-2 flex absolute z-20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-center text-2xl font-bold duration-300 max-w-[185px]",
+        className,
+      )}
       aria-label={ariaLabel}
     >
       <div className="flex items-center justify-center gap-4 border border-white w-full ">
